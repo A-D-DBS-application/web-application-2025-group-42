14a78b0ade91cb47aa28d4ed4ad16f6472533d29
damonproot <damon.proot@ugent.be>
2025-11-07 12:47:08 +0100

    alle files

---
+++ app/config.py
@@ -0,0 +1,4 @@
+class Config: 
+    SECRET_KEY = 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:supabase42%21@db.cnoqicqyuapnbjcumaoq.supabase.co:5432/postgres'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
