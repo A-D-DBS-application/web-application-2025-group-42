a8317b505e84589e0c7c2548daf2b9cb2d47fcf7
tiemendejonghe <tiemen.dejonghe@ugent.be>
2025-11-24 16:24:06 +0100

    n

--- app/config.py
+++ app/config.py
@@ -1,4 +1,6 @@
 class Config:
-    SECRET_KEY = "changeme123"  # mag je zelf vervangen
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:supabase42!@db.cnoqicqyuapnbjcumaoq.supabase.co:5432/postgres?sslmode=require"
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = "changeme-but-keep-it-long"  # mag je zelf aanpassen
+    SQLALCHEMY_DATABASE_URI = (
+        "postgresql://postgres:supabase42!@db.cnoqicqyuapnbjcumaoq.supabase.co:5432/postgres?sslmode=require"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
