e1d785cfa1194291f32685f48dfc47659e3981ed
tiemendejonghe <tiemen.dejonghe@ugent.be>
2025-11-24 16:05:57 +0100

    c

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
-class Config: 
-    SECRET_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNub3FpY3F5dWFwbmJqY3VtYW9xIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MDg5OTQ5NCwiZXhwIjoyMDc2NDc1NDk0fQ.KqdePx9hD0Sf75HPsmpVIaDxit-5kGQlUU0MV-EMMdM'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:supabase42%@db.cnoqicqyuapnbjcumaoq.supabase.com:5432/postgres'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+class Config:
+    SECRET_KEY = "changeme123"  # mag je zelf vervangen
+SQLALCHEMY_DATABASE_URI = "postgresql://postgres:supabase42!@db.cnoqicqyuapnbjcumaoq.supabase.co:5432/postgres?sslmode=require"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
